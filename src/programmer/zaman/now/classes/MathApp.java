c60b75e5d3590e375f551f47b763fb17ae64072a
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-05-09 02:53:54 +0700

    java-standard-class- Math

---
+++ src/programmer/zaman/now/classes/MathApp.java
@@ -0,0 +1,14 @@
+package programmer.zaman.now.classes;
+
+public class MathApp {
+    public static void main(String[] args) {
+        var math = Math.min(1000,2000);
+        System.out.println(math);
+
+        var math2 = Math.max(1000,2000);
+        System.out.println(math2);
+
+        System.out.println(Math.PI);
+
+    }
+}
