bcd8ff4da5fff6e8f3349c2e5b674f1bb10c4c4c
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-05-08 17:01:38 +0700

    java-standard-class- StringBuilder

---
+++ src/programmer/zaman/now/classes/StringBuilderApp.java
@@ -0,0 +1,10 @@
+package programmer.zaman.now.classes;
+
+public class StringBuilderApp {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Satria ");
+        builder.append(" ");
+        builder.append("Mulya");
+    }
+}
