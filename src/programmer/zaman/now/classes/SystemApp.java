81e9269646cb7270acde377f5d353f5061fbb256
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-06-01 12:55:43 +0700

    java-standard-class- Date and Calender

---
+++ src/programmer/zaman/now/classes/SystemApp.java
@@ -0,0 +1,14 @@
+package programmer.zaman.now.classes;
+
+public class SystemApp {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+        System.out.println(System.getenv("USERDOMAIN_ROAMINGPROFILE"));
+        System.out.println(System.getenv("APP"));
+        System.gc();
+
+        System.exit(20);
+        System.out.println("hello");
+    }
+}
